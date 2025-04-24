3672d6682ecab35210364b8cdadbd63d6c4274a2
שיף חני < @GurSeminar.Domain>
2025-04-24 11:35:33 +0300

    add the basic project pages and home page

--- src/components/oldOrders.jsx
+++ src/components/oldOrders.jsx
@@ -17,9 +17,6 @@ import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
-import VpnKeyIcon from '@mui/icons-material/VpnKey';
-import CarpenterIcon from '@mui/icons-material/Carpenter';
-import HandymanIcon from '@mui/icons-material/Handyman';
 export const OldOrders = () => {
 
 
